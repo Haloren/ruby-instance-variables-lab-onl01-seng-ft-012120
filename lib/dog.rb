5bfcc994c033fdc7a22af262e738776024daf356
Kyle McKenna <mckennakyler@gmail.com>
2020-01-27 21:57:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,15 @@
 Class Dog 
-  @this_dogs_name
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end 
+  
+  def name
+    this_dogs_name
+  end   
 end 
 
-lassie = Dog.new 
 
-@this_dogs_name 
+lassie = Dog.new 
+lassie.name = "Lassie"
+
+lassie.name 
